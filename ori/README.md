c96bb8e4a912b09e8430d4df1964fb4d69d09a68
heon <siwmua0@naver.com>
2022-03-06 23:18:40 +0900

    captain

--- ori/README.md
+++ ori/README.md
@@ -1,4 +1,4 @@
-# projectvue
+# ori
 
 ## Project setup
 ```
---
+++ ori/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      hello
+    </div>
+    <button @click="btn()">click</button>
+  </div>
+</template>
+
+<script>
+
+import apple from './assets/test.js';
+alert(apple)
+
+
+export default {
+  name: 'App',
+  data() {
+    return{
+
+    }
+  },
+  methods: {
+    btn(){
+      alert("alter")
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
